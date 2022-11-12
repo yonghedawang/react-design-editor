29a79882a25253fef0d71ef824506139e18c59f5
jyh <343970500@qq.com>
2022-11-12 14:50:41 +0800

    简化代码，便于学习

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { LocaleProvider } from 'antd';
-import koKR from 'antd/lib/locale-provider/ko_KR';
-import enUS from 'antd/lib/locale-provider/en_US';
-import App from './App';
-import { register } from './serviceWorker';
-import i18next from 'i18next';
-import { i18nClient } from './i18n';
 
-const antResources = {
-	ko: koKR,
-	'ko-KR': koKR,
-	en: enUS,
-	'en-US': enUS,
-};
+import App from './App';
+
+
+
 
 const root = document.createElement('div');
 root.id = 'root';
 document.body.appendChild(root);
 
-const render = Component => {
-	const rootElement = document.getElementById('root');
-	ReactDom.render(
-		<AppContainer>{/* 热加载，可以去掉 */}
-			<LocaleProvider locale={antResources[i18next.language]}>
-				<Component />
-			</LocaleProvider>
-		</AppContainer>,
-		rootElement,
-	);
-};
 
-i18nClient();
-
-render(App);
-
-register();
-
-if (module.hot) {
-	module.hot.accept('./App', () => {
-		render(App);
-	});
-}
+const rootElement = document.getElementById('root');
+ReactDom.render(<App />, rootElement);
